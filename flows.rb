d8dc2b8a9c27a651f4bda07504f89ea66e8f6781
Takashi Ohida <pichon@gmail.com>
2021-08-28 23:12:03 +0900

    remove comments

--- flows.rb
+++ flows.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Flows < Formula
   desc ""
   homepage ""
-  url "https://ohida.net/flows/releases/flows-0.0.1.tgz"
-  sha256 ""
+  url "https://ohida.net/flows/releases/flows-0.0.1.tar.gz"
+  sha256 "502434c1e75626d3e2bb9378acad151bad1f9426d7107d1853b38e9aad181551"
   license ""
 
   # depends_on "cmake" => :build
@@ -19,31 +16,19 @@ class Flows < Formula
     system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
 
-    bin.install "flows"
-    bin.install_symlink "flows"
+    bin.install 'flows'
   end
 
   bottle do
     root_url "https://ohida.net/flows/releases"
-    sha256 big_sur: "efae7d9dce9f8c876388e257bab708d04c7da0078ff9f2eb190d84fd7ea9dd6a"
+    sha256 big_sur: "502434c1e75626d3e2bb9378acad151bad1f9426d7107d1853b38e9aad181551"
   end
 
   def pour_bottle?
-    # Only needed if this formula has to check if using the pre-built
-    # bottle is fine.
     true
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test flows`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/flows", "version"
   end
 end
